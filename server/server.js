aa82de4b8a8dc681a6d4cab0ce0c3f5fe11e079a
Yuval Gal <yuval.gal@lifetrackmed.com>
2023-06-21 13:41:34 +0100

    fixed

--- server/server.js
+++ server/server.js
@@ -1,17 +1,27 @@
+import {getUserData} from "./services/db.service.js";
 import {validateRequestData} from './middlewares/validationMiddleware.js';
 import { validateAuthorization } from './middlewares/authMiddleware.js';
 import { saveDataToFile } from './services/fs.service.js';
 import * as dotenv from 'dotenv'
 const app = express();
+
 import express from 'express';
 import bodyParser from 'body-parser';
 
 app.use(bodyParser.json());
 dotenv.config()
 
-app.get('/userEvents/:userId', (req, res) => {
+app.get('/userEvents/:userId', async (req, res) => {
         const userId = req.params.userId;
-        return res.send(`Received a GET HTTP method with userId: ${userId}`);
+        let resp = await getUserData(String(userId));
+        if (resp.rowCount > 0 ) {
+            resp = resp.rows[0];
+            return res.send(resp);
+        }
+        else {
+            return res.send("No result");
+        }
+
 });
 app.post('/liveEvent', validateAuthorization, (req, res, next) => {
     validateRequestData(req, res, () => {
